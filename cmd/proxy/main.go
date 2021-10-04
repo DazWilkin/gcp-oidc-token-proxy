ee925e7391af247d9cb9abcda86c075908545e9e
DazWilkin <daz.wilkin@gmail.com>
2021-10-04 11:24:32 -0700

    Just return the `access_token`

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -18,8 +18,9 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2"
 
+	"google.golang.org/api/idtoken"
 	"google.golang.org/grpc/credentials/oauth"
 )
 
@@ -28,6 +29,25 @@ const (
 )
 const (
 	scopeCloudPlatform string = "https://www.googleapis.com/auth/cloud-platform"
+)
+
+// GrantType represents the implicit GrantType enum in Google's Token Service Request type
+type GrantType int
+
+func (g GrantType) String() string {
+	switch g {
+	case AuthorizationCode:
+		return "authorization_code"
+	case RefreshToken:
+		return "refresh_token"
+	default:
+		panic(fmt.Sprintf("Unknown GrantType (%d)", int(g)))
+	}
+}
+
+const (
+	AuthorizationCode = iota
+	RefreshToken      = iota
 )
 
 var (
@@ -52,6 +72,7 @@ var (
 var (
 	apiKey string
 	creds  oauth.TokenSource
+	ts     oauth2.TokenSource
 )
 
 func init() {
@@ -66,27 +87,43 @@ func init() {
 	if os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
 		log.Error(nil, "Unable to find GOOGLE_APPLICATON_CREDENTIALS in the environment")
 	}
-	tokenSrc, err := google.DefaultTokenSource(context.Background(), scopeCloudPlatform)
+	// DefaultTokenSource uses GOOGLE_APPLICATION_CREDENTIALS
+	var err error
+	// ts, err = google.DefaultTokenSource(context.Background(), scopeCloudPlatform)
+	// aud := "https://ackal-healthcheck-server-2eynp5ydga-wl.a.run.app"
+	aud := "https://oauth2.googleapis.com/token"
+	ts, err = idtoken.NewTokenSource(context.Background(), aud)
 	if err != nil {
 		log.Error(err, "Unable to get default token source")
 		os.Exit(1)
 	}
 
-	creds = oauth.TokenSource{tokenSrc}
+	creds = oauth.TokenSource{ts}
 }
 
+// Client is a type that is not yet used
 type Client struct {
 	client http.Client
 	creds  oauth.TokenSource
 }
 
+// Request is a type that corresponds to Google's Token Service Request
+// https://developers.google.com/identity/toolkit/reference/securetoken/rest/v1/token
 type Request struct {
 	GrantType    string `json:"grant_type" description:"The type of token being sent"`
 	Code         string `json:"code" description:"Identity token to exchange for an access token"`
 	RefreshToken string `json:"refresh_token" description:"Refresh token to exchange for an access token"`
 }
 
+// Response is a type that corresponds to Google's Token Service Response
 // Values is a method that converts a TokenRequest into URL-encoded values
+type Response struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    string `json:"expires_in"`
+	TokenType    string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+}
+
 func (t *Request) Values() url.Values {
 	data := url.Values{}
 	data.Set("grant_type", t.GrantType)
@@ -116,7 +153,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		"Body", string(b),
 	)
 
-	tok, err := creds.Token()
+	// tok, err := creds.Token()
+	tok, err := ts.Token()
 	if err != nil {
 		log.Error(err, "Unable to get token from token source")
 		os.Exit(1)
@@ -126,21 +164,28 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		"token", tok,
 	)
 
-	if tok.Extra("id_token") == nil {
-		log.Error(nil, "Unable to determine ID token")
-	}
+	access_token := tok.AccessToken
+	log.Info("Access Token",
+		"access_token", access_token,
+	)
 
-	id_token := tok.Extra("id_token").(string)
+	// rqst := Request{
+	// 	GrantType: "authorization_code",
+	// 	Code:      access_token,
+	// }
 
-	rqst := Request{
-		GrantType: "authorization_code",
-		Code:      id_token,
-	}
-	client := *http.DefaultClient
-	resp, err := client.PostForm(fmt.Sprintf("%s?key=%s", method, apiKey), rqst.Values())
-	if err != nil {
-		log.Error(err, "Unable to POST against Google Secure Token endpoint")
-		return
+	// client := *http.DefaultClient
+
+	// resp, err := client.PostForm(fmt.Sprintf("%s?key=%s", method, apiKey), rqst.Values())
+	// if err != nil {
+	// 	log.Error(err, "Unable to POST against Google Secure Token endpoint")
+	// 	return
+	// }
+
+	resp := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: access_token,
 	}
 
 	j, err := json.Marshal(resp)
